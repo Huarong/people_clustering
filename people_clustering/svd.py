098c75d0b5d1113fb7b078404c54e7aeeb50d559
Huo Huarong <huohuarong@gmail.com>
2014-04-26 21:39:12 +0800

    use svd.py

---
+++ people_clustering/svd.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import numpy as np
+
+import util
+
+
+def get_topicMatrix(Matrix, topicNum, fill = True):
+    h,w = Matrix.shape
+    if(True==fill):
+        add = float(np.sum(Matrix))/(h*w)
+        tmp = np.zeros((h,w), dtype=np.float32)
+        for i in range(h):
+            for j in range(w):
+                if(0==Matrix[i][j]):
+                    tmp[i][j] += add
+                else:
+                    tmp[i][j] += float(Matrix[i][j])
+        Matrix = tmp
+    m = min(h,w)
+    if (m<topicNum):
+        print "topicNum is large than the minor dimension of the Matrix!"
+        sys.exist()
+    u,s,v = np.linalg.svd(Matrix)
+    l = len(s)
+    sm = np.zeros((l,l))
+    for i in range(topicNum):
+        sm[i][i] = s[i]
+
+    if(h<w):
+        Matrix2 = (u.dot(sm)).dot(v[0:h,0:w])
+    else:
+        Matrix2 = (u[0:h,0:w].dot(sm)).dot(v)
+
+    return Matrix2
+
+
+def test_get_topicMatrix():
+    n = np.array([[4, 3, 2, 1, 0],[0, 3, 2, 1, 0],[4, 4, 2, 7, 0],[1, 0, 0, 0, 0],[0, 0, 0, 1, 1]])
+    tm = get_topicMatrix(n, 2,False)
+    print tm
+
+
+def load_matrix(path):
+    matrix = []
+    with open(path) as f:
+        row = [float(e) for e in f.readline().split()]
+        matrix.append(row)
+    return matrix
+
+def dump_matrix(matrix, path):
+    with open(path, 'wb') as out:
+        for row in matrix:
+            out.write(' '.join([str(e) for e in row]))
+            out.write(os.linesep)
+    print 'Finish writing matrix to %s' % path
+    return None
+
+
+def main():
+    matrix_dir = os.path.join(util.ROOT, 'pickle/matrix/')
+    svd_dir = os.path.join(util.ROOT, 'pickle/svd_matrix/')
+    if not os.path.exists(svd_dir):
+        os.makedirs(svd_dir)
+
+    for file_name in os.listdir(matrix_dir):
+        name = file_name.split('.')[0]
+        matrix_path = os.path.join(matrix_dir, file_name)
+        matrix = load_matrix(matrix_path)
+        svd_matrix = get_topicMatrix(np.array(matrix), len(matrix))
+        svd_path = os.path.join(svd_dir, '%s.matrix' % name)
+        dump_matrix(svd_matrix, svd_path)
+    return None
+
+
+if __name__ == '__main__':
+    # test_get_topicMatrix()
+    main()
--- people_clustering/util.py
+++ people_clustering/util.py
@@ -2,11 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import time
 import logging
-
-import numpy as np
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print ROOT
@@ -39,40 +36,6 @@ def init_log(logname, filename, level=logging.DEBUG, console=True):
         logger.addHandler(console)
     return logger
 
-def get_topicMatrix(Matrix, topicNum, fill = True):
-    h,w = Matrix.shape
-    if(True==fill):
-        add = float(np.sum(Matrix))/(h*w)
-        tmp = np.zeros((h,w), dtype=np.float32)
-        for i in range(h):
-            for j in range(w):
-                if(0==Matrix[i][j]):
-                    tmp[i][j] += add
-                else:
-                    tmp[i][j] += float(Matrix[i][j])
-        Matrix = tmp
-    m = min(h,w)
-    if (m<topicNum):
-        print "topicNum is large than the minor dimension of the Matrix!"
-        sys.exist()
-    u,s,v = np.linalg.svd(Matrix)
-    l = len(s)
-    sm = np.zeros((l,l))
-    for i in range(topicNum):
-        sm[i][i] = s[i]
-
-    if(h<w):
-        Matrix2 = (u.dot(sm)).dot(v[0:h,0:w])
-    else:
-        Matrix2 = (u[0:h,0:w].dot(sm)).dot(v)
-
-    return Matrix2
-
-def test_get_topicMatrix():
-    n = np.array([[4, 3, 2, 1, 0],[0, 3, 2, 1, 0],[4, 4, 2, 7, 0],[1, 0, 0, 0, 0],[0, 0, 0, 1, 1]])
-    tm = get_topicMatrix(n, 2,False)
-    print tm
-
 
 def timer(func, logger=None):
     def wrapper(*arg, **kw):
@@ -86,7 +49,3 @@ def timer(func, logger=None):
             print infomation
         return None
     return wrapper
-
-
-if __name__=="__main__":
-	test_get_topicMatrix()
