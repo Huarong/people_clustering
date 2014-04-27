3dfdf774e162dd71ceceba121256d1777e1f6d88
horton2009 <horton.hu.5@gmail.com>
2014-04-27 12:12:45 +0800

    horton: delet folder --  mycluster

--- people_clustering/Evaluate.py
+++ people_clustering/Evaluate.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import subprocess
-cmd =  ['java','../result/wepsEvaluation.jar es.nlp.uned.weps.evaluation.SystemScorer','',\
+cmd =  ['java','-cp','../result/wepsEvaluation.jar', 'es.nlp.uned.weps.evaluation.SystemScorer',\
 '../result/truth_official/','../result/test_system/','../result/','-ALLMEASURES','-AllInOne',\
- '-OneInOne', '-Combined', '-average']
+ '-OneInOne', '-Combined', '-average','-true']
 
 subprocess.call(cmd)
